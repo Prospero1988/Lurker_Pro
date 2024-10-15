dfa57d2c38d3252f2154ab7c800770315543fdb4
Prospero1988 <arek.kein@gmail.com>
2024-10-15 11:55:39 +0200

    release update

--- Lurker_PRO_v3.py
+++ Lurker_PRO_v3.py
@@ -29,16 +29,16 @@ def get_directory_from_user():
             print(f"Error: {e}")
 
 def search_folders(default_dir, pattern, output_path, progress_bar):
-    #print(f"Searching in: {default_dir}")  # Debug: wyświetl ścieżkę katalogu
-    #print(f"Using pattern: {pattern}")     # Debug: wyświetl używany wzorzec
+    # print(f"Searching in: {default_dir}")  # Debug: display the directory path
+    # print(f"Using pattern: {pattern}")     # Debug: display the pattern being used
     for root, dirs, files in os.walk(default_dir):
-        #print(f"Checking directory: {root}")  # Debug: wyświetl przeszukiwany katalog
+        # print(f"Checking directory: {root}")  # Debug: display the directory being searched
         for file in files:
             progress_bar.update(1)
             file_path = os.path.join(root, file)
-            #print(f"Checking file: {file_path}")  # Debug: wyświetl przeszukiwany plik
+            # print(f"Checking file: {file_path}")  # Debug: display the file being searched
             if pattern.search(file):
-                #print(f"Match found: {file_path}")  # Debug: wyświetl znaleziony plik
+                # print(f"Match found: {file_path}")  # Debug: display the found file
                 destination_path = os.path.join(output_path, os.path.basename(file_path))
                 if not os.path.exists(destination_path):
                     try:
@@ -97,41 +97,50 @@ The script also searches the contents of RAR and ZIP archives.
 # Clear the console and display the ASCII art logo
 subprocess.call('cls' if os.name == 'nt' else 'clear', shell=True)
 print('')
-ascii_art= text2art("LURKER+PRO")
+ascii_art = text2art("LURKER+PRO")
 print(ascii_art)
 print('')
 
+# ANSI color
+COLORS = ['\033[38;5;46m',    # Green
+          '\033[38;5;196m',   # Red
+          '\033[38;5;214m'    # Orange
+         ]
+RESET = '\033[0m'
 
 # Folder management
-default_dir = r'\\data02.celon.local\NMR'
+default_dir = os.getcwd()
 try:
-    check_dir = input(f"\nDo you want to search the default NMR disk folder?\nChoose [n] to specify a different path.\n\n{default_dir} [y/n]: ").strip().lower()
+    check_dir = input(f"\nThe search will be performed in the default folder: {COLORS[2]}{default_dir}{RESET}\n"
+                      f"Choose {COLORS[0]}[y]{RESET} to specify a different path or {COLORS[1]}[n]{RESET} to"
+                      f" proceed in default folder.\n\n"
+                      f"Do you want to specify a folder to search? [{COLORS[0]}[y]{RESET}/{COLORS[1]}[n]{RESET}]: ").strip().lower()
 except Exception as e:
     print(f"An error occurred while trying to read the path: {e}")
     exit()
 
 while True:
-    if check_dir == 'y':
+    if check_dir == 'n':
         while True:
             if not os.path.exists(default_dir):
-                print("The default folder does not exist. You must manually define a folder to search.")
+                print(f"{COLORS[1]}The default folder does not exist. You must manually define a folder to search.{RESET}")
                 default_dir = get_directory_from_user()
             else:
-                print(f"\nThe folder to be searched will be: {default_dir}")
+                print(f"\nThe folder to be searched will be: {COLORS[2]}{default_dir}{RESET}")
                 break
         break
-    elif check_dir == 'n':
+    elif check_dir == 'y':
         default_dir = get_directory_from_user()
-        print(f"The folder to be searched will be: {default_dir}")
+        print(f"The folder to be searched will be: {COLORS[2]}{default_dir}{RESET}")
         break
     else:
-        check_dir = input("Choose only [y/n]: ").strip().lower()
+        check_dir = input(f"\n{COLORS[1]}Choose only [y/n]: {RESET}").strip().lower()
 
 # Creating a folder where the found files will be saved.
 output_dir = 'output'
 output_path = os.path.join(os.getcwd(), output_dir)
 os.makedirs(output_path, exist_ok=True)
-print(f"The found files will be saved to the folder: {os.path.abspath(output_path)}")
+print(f"The found files will be saved to the folder: {COLORS[2]}{os.path.abspath(output_path)}{RESET}")
 
 # File search module
 phrases_input = input("\nEnter the name of the file, files, or keywords. \nSeparate them by commas: ")
